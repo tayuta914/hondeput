607c24db8db8902a06b337fb8b315914855d52a8
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-10-13 16:12:26 +0900

    詳細機能

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,6 @@
 #     https: false
 #   )
 # end
+
+
+Refile.secret_key = 'c4fb8551cbe91fac1bf8851642ae3a2c4dfeb4d2548201edb815ec916074463c88a12b10bc4ec4569a48fda8a0e6ea91cdb7478509dc900576c34b3cb8fd8fcd'
