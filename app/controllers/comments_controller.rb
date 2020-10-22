d46dc5cb929d8dfbc01d357a33a04b450498a87f
tayuta914 <70478152+tayuta914@users.noreply.github.com>
2020-10-22 10:52:36 +0900

    コメント機能　改

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,24 +1,18 @@
 class CommentsController < ApplicationController
-  before_action :authenticate_user!, expect: [:new, :create]
   
-    def create
-      @post = Post.find(params[:post_id])
-      @comment = @post.comments.build(comment_params)
-      @comment.user_id = current_user.id
-      if @comment.save
-        render :index
-      end
-    end
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to "/posts/#{comment.post.id}" 
+  end
 
-    def destroy
-      @comment = Comment.find(params[:id])
-      if @comment.destroy
-        render :index
-      end
-    end
+  def destroy
+    @comment = Comment.find_by(params[:id])
+    @comment.destroy
+    redirect_to posts_path, notice: "コメントを削除しました。" 
+  end
 
-    private
-    def comment_params
-      params.require(:comment).permit(:content, :post_id, :user_id)
-    end
+  private
+  def comment_params
+    params.require(:comment).permit(:content, :post_id, :user_id).merge(user_id: current_user.id, post_id: params[:post_id])
+  end
 end
